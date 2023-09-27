4061e6136c37540c0c5915fcdd388a031de3bf20
Youneskhavan <146028683+Youneskhavan@users.noreply.github.com>
2023-09-27 14:02:20 +0330

    Method_CallByRefrence_The method has two outputs
    Taking three numbers from the user and showing their sum and average

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Call_by_refrence;
+using System;
+
+
+
+
+int Sum;
+int Avg;
+Console.WriteLine("Enter the grade of lesson 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the grade of lesson 2");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the grade of lesson 3");
+int c = Convert.ToInt32(Console.ReadLine()); 
+
+Teacher.AvgSum(a,b,c,out Sum, out Avg );
+
+Console.WriteLine("Sum : {0}\t Avg : {1} ",Sum,Avg);
---
+++ Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_by_refrence
+{
+    internal static class Teacher
+    {
+
+        public static void AvgSum(int a, int b, int c, out int Sum, out int Avg)
+        {
+            Sum = 0;
+            Avg = 0;
+
+            Sum = a + b + c;
+            Avg = Sum / 3;
+        }
+
+
+    }
+}
